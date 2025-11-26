fcddbdf2962729bab178529669490702012b8e69
salobando <salobando2023@gmail.com>
2025-11-26 16:44:25 -0500

    Laboratorio operadores Java

--- src/JavaBasicos/Operadores.java
+++ src/JavaBasicos/Operadores.java
@@ -1,4 +1,16 @@
 package JavaBasicos;
 
 public class Operadores {
+    public static void operadores(){
+        boolean tieneDinero = true;
+        boolean tieneTarjeta = true;
+        boolean estaHaciedoFrio = true;
+        //AND
+        boolean puedeComprar = tieneDinero && tieneTarjeta;
+
+        int edad= 15;
+        boolean puedeEntrar = (edad >= 18) && estaHaciedoFrio;
+        estaHaciedoFrio = false;
+        boolean puedeEntrar2 = (edad >= 18) || estaHaciedoFrio;
+    }
 }
---
+++ src/LabJavaOperadores/ValidadorPerfilUsuario.java
@@ -0,0 +1,40 @@
+package LabJavaOperadores;
+
+import java.util.Scanner;
+
+public class ValidadorPerfilUsuario {
+    public static void datos(){
+        Scanner scanner = new Scanner(System.in);
+        String nombreUsuario;
+        int edad;
+        double saldoCuenta;
+        boolean esPremium = true;
+
+        System.out.println("Ingresa tu nombre: ");
+        nombreUsuario = scanner.nextLine();
+        System.out.println("Ingresa tu edad: ");
+        edad = scanner.nextInt();
+        System.out.println("Ingresa saldo de la cuenta: ");
+        saldoCuenta = scanner.nextDouble();
+        System.out.println("Eres Premium?:(true/false) ");
+        esPremium = scanner.nextBoolean();
+
+        System.out.println("\n**********INFORMACIÓN*********");
+        //accesoExclusivo
+        boolean accesoExclusivo = (edad >= 18) && esPremium;
+        System.out.println("Acceso Exclusivo: " + accesoExclusivo);
+
+        //calificaPromocion
+        boolean calificaPromocion = (saldoCuenta > 1000) || (edad < 25) ;
+        System.out.println("Califica a la Promociono: " + calificaPromocion);
+
+        //perfilIncompleto
+        boolean perfilIncompleto = (saldoCuenta == 0) || (!nombreUsuario.equals("invitado"));
+        System.out.println("Perfil Incompleto: " + perfilIncompleto);
+
+        //perfilActivoTotal
+        boolean perfilActivoTotal = (!perfilIncompleto) && (accesoExclusivo || calificaPromocion);
+        System.out.println("Perfil Activo: " +perfilActivoTotal);
+    }
+
+}
